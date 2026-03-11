46e6deacf0499e2db30e2be51a86b59b3590c96d
straht-lcc <straht@my.lanecc.edu>
2026-03-11 13:40:31 -0700

    Add files via upload

---
+++ budget_tracker1.py
@@ -0,0 +1,10 @@
+# Monthly Budget Tracker
+# Intro to Python Project - Initial Commit (March 2026)
+
+print("=== Simple Monthly Budget Tracker ===")
+print("Getting started on the project...\n")
+
+# MVP 2: Ask for monthly budget
+budget = float(input("Enter your monthly budget: $"))
+print(f"\nYour monthly budget has been set to ${budget:.2f}")
+print("More features (adding expenses + summary) will be added soon!")
